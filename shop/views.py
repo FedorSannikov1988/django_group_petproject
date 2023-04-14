87cb7297c18cf4b97b620f5b6d6c0cb807c09643
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-04-14 12:27:02 +0300

    made in a modular version of html templates: index , sitemap , about_us

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,6 @@
 from django.template.response import TemplateResponse
 from django.shortcuts import render
 
-# Create your views here.
 
 def index(request):
     return render(request, 'index.html')
