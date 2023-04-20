10aff793e3ccaf8c7f63bd268b4805e36f6fa090
Artem_Fedotov <fedotoff144@gmail.com>
2023-04-20 23:27:37 +0300

    Add base_entry.html, login.html and register.html. Work completed

--- shop/views.py
+++ shop/views.py
@@ -5,8 +5,18 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
 def sitemap(request):
     return render(request, 'sitemap.html')
 
+
 def about_us(request):
     return render(request, 'about_us.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
--- store/urls.py
+++ store/urls.py
@@ -6,9 +6,13 @@ from django.urls import path, re_path
 from shop.views import index
 from shop.views import sitemap
 from shop.views import about_us
+from shop.views import login
+from shop.views import register
 
 urlpatterns = [
     path("", index, name='index'),
     path("sitemap", sitemap, name='sitemap'),
     path("about_us", about_us, name='about_us'),
+    path("login", login, name='login'),
+    path("register", register, name='register'),
 ]
