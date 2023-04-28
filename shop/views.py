afbcbbafe1926251f52dfea4ba5c3cda32440026
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-04-28 19:58:53 +0300

    created the first version of the database for discussion + fixed a small bug

--- shop/views.py
+++ shop/views.py
@@ -6,27 +6,27 @@ from shop.models import SoftwareCategory, Software, FeaturesSoftware, Developmen
 
 def index(request):
     context = {
-    "all_soft": Software.objects.all(),
-    "software_category": SoftwareCategory.objects.all(),
-    "software_operating_systems": Software.objects.filter(category__name='Операционные системы'),
-    "software_office": Software.objects.filter(category__name='Офисное ПО'),
-    "software_antivirus_protection": Software.objects.filter(category__name='Антивирусная защита')
+        "all_soft": Software.objects.all(),
+        "software_category": SoftwareCategory.objects.all(),
+        "software_operating_systems": Software.objects.filter(category__name='Операционные системы'),
+        "software_office": Software.objects.filter(category__name='Офисное ПО'),
+        "software_antivirus_protection": Software.objects.filter(category__name='Антивирусная защита')
     }
     return render(request, 'index.html', context)
 
 
 def sitemap(request):
     context = {
-    "software_category": SoftwareCategory.objects.all(),
-    "software_operating_systems": Software.objects.filter(category__name='Операционные системы'),
-    "software_office": Software.objects.filter(category__name='Офисное ПО'),
-    "software_antivirus_protection": Software.objects.filter(category__name='Антивирусная защита')
+        "software_category": SoftwareCategory.objects.all(),
+        "software_operating_systems": Software.objects.filter(category__name='Операционные системы'),
+        "software_office": Software.objects.filter(category__name='Офисное ПО'),
+        "software_antivirus_protection": Software.objects.filter(category__name='Антивирусная защита')
     }
     return render(request, 'sitemap.html', context)
 
 
 def about_us(request):
     context = {
-    "development_team": DevelopmentTeam.objects.all()
+        "development_team": DevelopmentTeam.objects.all()
     }
     return render(request, 'about_us.html', context)
