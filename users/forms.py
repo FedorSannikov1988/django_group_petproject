d9a9c932dcd37ef6622dda043ee74f9554bc02d7
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-05-07 22:53:37 +0300

    another alternative to creating a form

--- users/forms.py
+++ users/forms.py
@@ -2,11 +2,22 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
-
+'''
 class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+'''
+
+# или другой вариант:
+
+from django import forms
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(label="Логин")
+    password = forms.CharField(max_length=10, widget=forms.PasswordInput)
+    field_order = ["username", "password"]
 
 
 class UserRegisterForm(UserCreationForm):
