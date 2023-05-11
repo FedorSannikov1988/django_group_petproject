fff07767f9dba9920c1fa7f47a1cf0440ef14f96
fedotoff144 <fedotoff144@gmail.com>
2023-05-11 16:54:43 +0300

    try change field on login page

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,6 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, User
 from users.models import User
 from django import forms
 
-
 '''
 class UserLoginForm(AuthenticationForm):
     class Meta:
@@ -10,13 +9,14 @@ class UserLoginForm(AuthenticationForm):
         fields = ('username', 'password')
 
 '''
+
+
 # или другой вариант:
 
 class UserLoginForm(forms.Form):
-        username = forms.CharField(label="Логин")
-        password = forms.CharField(max_length=20, widget=forms.PasswordInput)
-        field_order = ["username", "password"]
-
+    username = forms.CharField(label="Логин")
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    field_order = ["username", "password"]
 
 
 class UserRegisterForm(UserCreationForm):
@@ -26,6 +26,6 @@ class UserRegisterForm(UserCreationForm):
 
 
 class UserProfileForm(UserChangeForm):
-        class Meta:
-            model = User
-            fields = ('first_name', 'last_name', 'image', 'username', 'email')
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'image', 'username', 'email')
