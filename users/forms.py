61ac6a3e768cd240c0218d8990ad024d883fff0a
fedotoff144 <fedotoff144@gmail.com>
2023-05-12 16:57:33 +0300

    edited forms on my_account page

--- users/forms.py
+++ users/forms.py
@@ -1,21 +1,16 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.db.models import CharField
+
 from users.models import User
 from django import forms
 
-'''
-class UserLoginForm(AuthenticationForm):
-    class Meta:
-        model = User
-        fields = ('username', 'password')
-
-'''
-
-
-# или другой вариант:
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(label="Логин")
-    password = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    username = forms.CharField(label="Логин", widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Имя пользователя'}))
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                                'placeholder': 'Пароль'}))
     field_order = ["username", "password"]
 
 
@@ -26,6 +21,32 @@ class UserRegisterForm(UserCreationForm):
 
 
 class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(
+        label="Имя",
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Имя'}))
+    last_name = forms.CharField(
+        label="Фамилия",
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Фамилия'}))
+    username = forms.CharField(
+        label="Логин",
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Логин'}))
+    email = forms.EmailField(
+        label="Эл.почта",
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Эл.почта'}))
+    image = forms.ImageField(
+        label="Фото",
+        widget=forms.FileInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Фото'}))
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'image', 'username', 'email')
