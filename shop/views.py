3a68c9b46dd416a61a960adabc0fc7e9e1c0a24a
FedorSannikov1988 <111923149+FedorSannikov1988@users.noreply.github.com>
2023-05-13 18:28:15 +0300

    Revert "add fixture FAQ. add table faq. faq.html load from table"

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.template.response import TemplateResponse
 from django.shortcuts import render
 
-from shop.models import SoftwareCategory, Software, DevelopmentTeam, FAQ
+from shop.models import SoftwareCategory, Software, DevelopmentTeam
 
 # глобальные переменные/данные:
 
@@ -53,7 +53,6 @@ def faq(request):
 
     context = {
         'page_title': title_faq + title_for_basic_template,
-        'faq': FAQ.objects.all(),
     }
     return render(request, 'faq.html', {**context, **data_for_basic_template})
 
