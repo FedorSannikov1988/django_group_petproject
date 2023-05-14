cef5733f45e442ddfa7a58e2f1f246d4903b9a5d
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-05-14 10:40:50 +0300

    microscopic changes and the need to switch yf to another branch

--- shop/models.py
+++ shop/models.py
@@ -20,8 +20,8 @@ class Software(models.Model):
     category = models.ForeignKey(to=SoftwareCategory, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Software: {self.name} '\
-               f'| Price: {self.price} '\
+        return f'Software: {self.name} ' \
+               f'| Price: {self.price} ' \
                f'| Quantity : {self.quantity} '
 
 
@@ -34,9 +34,9 @@ class FeaturesSoftware(models.Model):
     software = models.ForeignKey(to=Software, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Operating System: {self.operating_system} '\
-               f'| Video Card: {self.video_card} '\
-               f'| Hard disk MB: {self.hard_disk_mb} '\
+        return f'Operating System: {self.operating_system} ' \
+               f'| Video Card: {self.video_card} ' \
+               f'| Hard disk MB: {self.hard_disk_mb} ' \
                f'| Min RAM MB: {self.min_ram_mb} '
 
 
@@ -51,14 +51,15 @@ class DevelopmentTeam(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to='images_development_team')
 
     def __str__(self):
-        return f'Firstname: {self.firstname} '\
-               f'| Lastname: {self.lastname} '\
+        return f'Firstname: {self.firstname} ' \
+               f'| Lastname: {self.lastname} ' \
                f'| Patronymic: {self.patronymic} '
+
 
 class FAQ(models.Model):
     question = models.TextField(null=True, blank=True)
     answer = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return f'Question: {self.question} '\
+        return f'Question: {self.question} ' \
                f'| Answer: {self.answer}  '
