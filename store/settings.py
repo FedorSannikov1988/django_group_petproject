257fff8a041d8793fc421f813acc93f099e9b56c
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-05-20 14:37:56 +0300

    Merge branch Anatoly_Stupin

--- store/settings.py
+++ store/settings.py
@@ -4,8 +4,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# Потом нужно сделать так что бы ключ не было видно !
-# Запихнуть в отдельный файл и включить данный файл в gitinit !
 SECRET_KEY = 'django-insecure-0^o=sxfb5_6#jpo&*)jsffrat^*yo#f-+)p(aiijo7!kp@_&ym'
 
 # SECURITY WARNING: don't run with debug turned on in production!
