5c2d798bd161c585ce2cc25de8f76dc565d543e8
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-05-20 14:39:39 +0300

    Merge branch Anatoly_Stupin

---
+++ shop/migrations/0002_cart_user.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('shop', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='cart',
+            name='user',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
