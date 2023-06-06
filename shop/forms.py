ae851fb7ee8fcd95c0b424ac6b4377c4232eac54
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-06-06 22:53:18 +0300

    all work + small changes

--- shop/forms.py
+++ shop/forms.py
@@ -7,23 +7,13 @@ class ShopFaqForm(forms.ModelForm):
     question_timestamp = models.DateTimeField(auto_now_add=True, null=True)
     userquestion = forms.CharField(label="Текст сообщения:",
         widget=forms.Textarea(
-        attrs={'max_length': 500,
-               'class': 'form-control',
-               'id': 'input-comment',
-               'placeholder': 'Текст сообщения',
-               'rows': 10,
-               'name': 'comment'}))
+            attrs={'max_length': 500,
+                   'class': 'form-control',
+                   'id': 'input-comment',
+                   'placeholder': 'Текст сообщения',
+                   'rows': 10,
+                   'name': 'comment'}))
 
     class Meta:
         model = UsersQuestions
-
         fields = ('userquestion', 'upload')
-#-?
-        widgets = {forms.Textarea(
-        attrs={'max_length': 500,
-               'class': 'form-control',
-               'id': 'input-comment',
-               'placeholder': 'Текст сообщения',
-               'rows': 10,
-               'name': 'comment'})
-        }
