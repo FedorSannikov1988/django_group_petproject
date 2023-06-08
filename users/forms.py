cdaa51fb3e7412fa82fa2b27f105486406f5eb19
fedotoff144 <fedotoff144@gmail.com>
2023-06-08 23:27:40 +0300

    rename error's fields part 2

--- users/forms.py
+++ users/forms.py
@@ -30,12 +30,12 @@ class UserRegisterForm(UserCreationForm):
                    'placeholder': 'Фамилия'}))
     username = forms.CharField(
         required=False,
-        label="Логин",
+        label="Имя пользователя",
         widget=forms.TextInput(
             attrs={'class': 'form-control',
                    'placeholder': 'Логин'}))
     email = forms.EmailField(
-        label="Эл.почта",
+        label="email",
         widget=forms.EmailInput(
             attrs={'class': 'form-control',
                    'placeholder': 'Эл.почта'}))
@@ -45,24 +45,16 @@ class UserRegisterForm(UserCreationForm):
             attrs={'class': 'form-control',
                    'placeholder': 'Пароль'}))
     password2 = forms.CharField(
-        label='Пароль',
+        label="Подтвердить пароль",
         widget=forms.PasswordInput(
             attrs={'class': 'form-control',
                    'placeholder': 'Подтвердите пароль'}))
+
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name',
                   'username', 'email', 'password1', 'password2']
-
-
-        # labels = {                  trying rename variables in errors messages
-        #     'first_name': 'Имя',
-        #     'last_name': 'Фамилия',
-        #     'username': 'Имя пользователя',
-        #     'password1': 'Пароль 1',
-        #     'password2': 'Пароль 2',
-        # }
 
 
 class UserProfileForm(UserChangeForm):
