ff5757e30ad296c0d85ddf63c04f7d0c77c335e2
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-06-16 17:50:13 +0300

    after merging and correction

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
-from shop.forms import ShopFaqForm
-from shop.models import SoftwareCategory, Software, FeaturesSoftware, DevelopmentTeam, FAQ, Cart
-from shop.models import SoftwareCategory, Software, DevelopmentTeam, FAQ, Cart
 from django.core.paginator import Paginator
+from shop.forms import ShopFaqForm
+from shop.models import FeaturesSoftware, SoftwareCategory, Software, DevelopmentTeam, FAQ, Cart
 
 
 def title_for_basic_template():
@@ -25,23 +24,13 @@ def data_for_basic_template(request):
     return data
 
 
-def all_soft():
-    data = {
-        # "all_soft": Software.objects.all(),
-        # "software_operating_systems": Software.objects.filter(category__name='Операционные системы'),
-        # "software_office": Software.objects.filter(category__name='Офисное ПО'),
-        # "software_antivirus_protection": Software.objects.filter(category__name='Антивирусная защита')
-    }
-    return data
-
-
 def index(request):
     title_index = 'Главная страница - '
 
     context = {
         "page_title": title_index + title_for_basic_template()
     }
-    return render(request, 'index.html', {**context, **data_for_basic_template(request), **all_soft()})
+    return render(request, 'index.html', {**context, **data_for_basic_template(request)})
 
 
 def sitemap(request):
@@ -91,6 +80,7 @@ def product(request, software_id):
     category = SoftwareCategory.objects.get(id=software.category_id)
     context = {
         'page_title': title_product + title_for_basic_template(),
+        'software_id': software_id,
         'software_name': software.name,
         'software_image': software.image.url,
         'software_price': software.price,
@@ -98,7 +88,7 @@ def product(request, software_id):
         'software_category_id': software.category_id,
         'software_category_name': category.name,
     }
-    return render(request, 'product.html', context)
+    return render(request, 'product.html', {**context, **data_for_basic_template(request)})
 
 
 def products_catalog(request, category_id=None, page_number=1):
@@ -111,12 +101,13 @@ def products_catalog(request, category_id=None, page_number=1):
     paginator = Paginator(software, per_page=6)
     software_paginator = paginator.page(page_number)
 
-    context = {"page_title": title_product_catalog + title_for_basic_template(),
-               "categories": SoftwareCategory.objects.all(),
-               "software": software_paginator,
-    }
+    context = {
+                "page_title": title_product_catalog + title_for_basic_template(),
+                "categories": SoftwareCategory.objects.all(),
+                "software": software_paginator
 
-    return render(request, 'catalog.html', context)
+    }
+    return render(request, 'catalog.html', {**context, **data_for_basic_template(request)})
 
 
 @login_required
@@ -130,7 +121,8 @@ def cart(request):
     for one_purchase in cart_user_small:
         featuresSoftware = FeaturesSoftware.objects.filter(id=one_purchase.software.id).first()
 
-        new_line = {'image': one_purchase.software.image,
+        new_line = {
+                    'image': one_purchase.software.image,
                     'name': one_purchase.software.name,
                     'software_id': one_purchase.software.id,
                     'one_purchase_id': one_purchase.id,
@@ -141,7 +133,8 @@ def cart(request):
                     'min_ram_mb': featuresSoftware.min_ram_mb,
                     'quantity_in_card': one_purchase.quantity,
                     'software_price': one_purchase.software.price,
-                    'one_purchase_sum': one_purchase.sum}
+                    'one_purchase_sum': one_purchase.sum
+        }
 
         cart_user_big.append(new_line)
 
@@ -151,7 +144,7 @@ def cart(request):
         'big_cart_total_sum': cart_user_small.total_sum,
         'page_title': title_cart + title_for_basic_template(),
     }
-    return render(request, 'cart.html', context)
+    return render(request, 'cart.html', {**context, **data_for_basic_template(request)})
 
 
 @login_required
