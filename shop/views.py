ddf60e58523d2dda03994bc0d47c0a83f7737de1
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-08-30 14:54:21 +0300

    changed the name

--- shop/views.py
+++ shop/views.py
@@ -3,10 +3,10 @@ from shop.forms import ShopFaqForm
 from django.contrib.messages import error
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
-from django.contrib.postgres.search import SearchQuery, \
-                                           SearchVector
 from django.shortcuts import render, \
                              HttpResponseRedirect
+from django.contrib.postgres.search import SearchQuery, \
+                                           SearchVector
 from shop.models import ImageCollectionForIndex,\
                         FeaturesSoftware, \
                         SoftwareCategory, \
@@ -25,7 +25,7 @@ def title_for_basic_template() -> str:
     This string is used in other functions
     to form page headers.
     """
-    text: str = 'Дипломная работа'
+    text: str = 'Пет-проект'
     return text
 
 
